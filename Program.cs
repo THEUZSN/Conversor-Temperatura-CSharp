2df79c4f67ad6227d3876294a30d4a91709cfc21
Matheus Serra Nascimento <ggmat304@gmail.com>
2025-02-15 21:18:50 -0300

    Add files via upload
    Commit...

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercício_1;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Digite o Primeiro nome");
+        var nome1 = Console.ReadLine();
+
+        Console.WriteLine("Digite o segundo nome");
+        var nome2 = Console.ReadLine();
+
+        Console.WriteLine("Digite o terceiro nome");
+        var nome3 = Console.ReadLine();
+
+        Console.WriteLine("Digite o ultimo nome");
+        var nome4 = Console.ReadLine();
+
+        Console.WriteLine(nome1.Reverse());
+    }
+
+}
